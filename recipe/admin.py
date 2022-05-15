0a41aa044ca6869d7876f054d2a3a253b6517f78
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-15 15:27:06 +0100

    feat: add summernote wysiwig to recipe instructions field

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('instructions')
