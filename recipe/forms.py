b569b3a6bcca97f5387fc46352f49ee7504711d6
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-21 16:06:19 +0100

    feat[recipe]: add more fields for recipe form

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,11 +1,15 @@
 from django.forms import ModelForm, modelformset_factory
+from django.utils.translation import gettext_lazy as _
 from .models import Recipe, Ingredient
 
 
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
-        fields = ('name', 'meal')
+        fields = ('name', 'author', 'meal', 'cooking_time')
+        labels = {
+            'name': _('Recipe name'),
+        }
 
 
 IngredientFormset = modelformset_factory(
