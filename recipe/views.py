74ba615cbd99a7ba6177169252cd86452a6600fd
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-21 12:02:39 +0100

    feat[recipe]: add view for recipe detail

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
 class RecipeList(generic.ListView):
     model = Recipe
-    template_name = 'recipes.html'
+    template_name = 'recipe/recipes.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug):
+        recipe = get_object_or_404(Recipe, slug=slug)
+
+        return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
