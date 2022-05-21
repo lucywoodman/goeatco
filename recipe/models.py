80af9c72c0ef01b34abf34c2ac36e0de921b462a
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-21 14:47:28 +0100

    feat[recipe]: simplify recipe and ingredient models

--- recipe/models.py
+++ recipe/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-DIET = (
-    (0, ''),
-    (1, 'Gluten free'),
-    (2, 'Dairy free'),
-    (3, 'Nut free'),
-    (4, 'Soy free'),
-)
+from django.contrib.auth.models import User
 
 MEAL = (
     (0, 'Breakfast'),
@@ -25,50 +18,26 @@ class LowerCaseField(models.CharField):
         return str(value).lower()
 
 
-class Unit(models.Model):
-    name = LowerCaseField(max_length=30, unique=True)
+class Recipe(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes')
+    cooking_time = models.DurationField()
+    meal = models.IntegerField(choices=MEAL, default=2)
 
     def __str__(self):
         return self.name
 
-
-class Category(models.Model):
-    name = LowerCaseField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
+    def get_ingredients(self):
+        return self.ingredients.all()
 
 
 class Ingredient(models.Model):
     name = LowerCaseField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, null=True, blank=True)
+    recipe = models.ForeignKey(
+        Recipe, related_name='ingredients', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
-
-
-class RecipeRequirement(models.Model):
-    qty = models.DecimalField(max_digits=4, decimal_places=2)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, null=True)
-    ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return f'{self.qty} {self.unit} {self.ingredient}'
-
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    diet = models.IntegerField(choices=DIET, default=0)
-    source = models.URLField()
-    cooking_time = models.DurationField()
-    meal = models.IntegerField(choices=MEAL, default=2)
-    ingredients = models.ManyToManyField(
-        RecipeRequirement)
-    instructions = models.TextField()
-
-    def __str__(self):
-        return self.title
