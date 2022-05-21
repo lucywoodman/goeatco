e2d8160896d183e5d6bce72083a50971bae82b26
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-21 15:39:54 +0100

    feat[recipe]: build out recipe views for formsets (test)

--- recipe/views.py
+++ recipe/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
-from .models import Recipe
+from django.shortcuts import redirect, get_object_or_404, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView
+from .models import Recipe, Ingredient
+from .forms import RecipeForm, IngredientFormset
 
 
-class RecipeList(generic.ListView):
+class RecipeList(ListView):
     model = Recipe
-    template_name = 'recipe/recipes.html'
-    paginate_by = 6
+    template_name = 'recipe/recipe_list.html'
 
 
-class RecipeDetail(View):
+class RecipeAdd(TemplateView):
+    template_name = 'recipe/recipe_add.html'
+
+    def get(self, *args, **kwargs):
+        recipeform = RecipeForm(queryset=Recipe.objects.none())
+        formset = IngredientFormset(queryset=Ingredient.objects.none())
+        return self.render_to_response({'ingredient_formset': formset})
+
+    def post(self, *args, **kwargs):
+        recipeform = RecipeForm(data=self.request.POST)
+        formset = IngredientFormset(data=self.request.POST)
+        if recipeform.is_valid() and formset.is_valid():
+            recipe = recipeform.save()
+            for form in formset:
+                ingredient = form.save(commit=False)
+                ingredient.recipe = recipe
+                ingredient.save()
+            return redirect(reverse_lazy('recipe_list'))
+
+        return self.render_to_response({'ingredient_formset': formset})
+
+
+class RecipeDetail(TemplateView):
 
     def get(self, request, slug):
         recipe = get_object_or_404(Recipe, slug=slug)
