24349d3fef7e72119500e25145bada832260b0f5
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-21 12:03:06 +0100

    feat[recipe]: add url path for recipe detail

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
