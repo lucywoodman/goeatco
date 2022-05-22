f9f260d3ef17c825cd9bb3f835919b2a949aca70
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-22 11:28:22 +0100

    style[recipe]: add comments and code credit to explain script

--- static/js/addFormFields.js
+++ static/js/addFormFields.js
@@ -1,11 +1,26 @@
+/**
+ * Creating dynamic formsets with Django and JavaScript to allow adding of
+ * extra ingredients to recipes.
+ * 
+ * Credit: https://www.brennantymrak.com/articles/django-dynamic-formsets-javascript.html
+ *  */
+
+// create variables from form elements.
 let ingredientForm = document.querySelectorAll(".ingredient-form")
 let container = document.querySelector("#form-container")
 let addButton = document.querySelector("#add-form")
 let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
-
 let formNum = ingredientForm.length-1
+
+// add event listener on the relevant button.
 addButton.addEventListener('click', addForm)
 
+/**
+ * Adds extra ingredient fields to the add recipe form to allow
+ * the connecting of multiple ingredients to one recipe.
+ * 
+ * @param {event} e - prevents default button behaviour.
+ */
 function addForm(e){
     e.preventDefault()
 
