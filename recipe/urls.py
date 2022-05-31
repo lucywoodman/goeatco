7fbe9b38d3dbd52dfbbbc3337ff6084dfb98d8fb
Lucy Woodman <lucyjwoodman@gmail.com>
2022-05-31 08:20:33 +0100

    feat[recipe]: add success page for after recipe form submission

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name='recipe_list'),
+    path('success/', views.SuccessView.as_view(), name='success'),
     path('add', views.RecipeAddView.as_view(), name='recipe_add'),
     path('<slug:slug>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
