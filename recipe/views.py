4edb916b46c52a23067067008d370842096c3d6a
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-01 12:14:24 +0100

    refac[recipe]: simplify code by removing loops and utilising atomic transactions
    `transaction.atomic()` is a better option here as it prevents the recipe from being
    saved if there are any issues with the inlineformsets. Else we get a saved recipe
    without any ingredients/instructions, and then are unable to save again due to the
    name and slug needing to be unique.
    
    I'm also using get_context_data to help reduce the number of lines required for the
    create and update views and streamline the code.
    
    Ref: https://medium.com/@adandan01/django-inline-formsets-example-mybook-420cc4b6225d

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.db import transaction
 from .models import Recipe, IngredientMeta, Instructions
 from .forms import RecipeForm, IngredientFormset, InstructionFormset
 
@@ -24,48 +25,30 @@ class RecipeCreateView(generic.CreateView):
     form_class = RecipeForm
     success_url = 'success/'
 
-    def get(self, *args, **kwargs):
-        # clear reference to self
-        self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        ingredient_form = IngredientFormset()
-        instruction_form = InstructionFormset()
-        return self.render_to_response(
-            self.get_context_data(
-                form=form,
-                ingredient_form=ingredient_form,
-                instruction_form=instruction_form
-            )
-        )
+    def get_context_data(self, **kwargs):
+        data = super(RecipeCreateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['ingredients'] = IngredientFormset(self.request.POST)
+            data['instructions'] = InstructionFormset(self.request.POST)
+        else:
+            data['ingredients'] = IngredientFormset()
+            data['instructions'] = InstructionFormset()
+        return data
 
-    def post(self, request, *args, **kwargs):
-        self.object = None
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        ingredient_form = IngredientFormset(self.request.POST)
-        instruction_form = InstructionFormset(self.request.POST)
-        if (form.is_valid() and ingredient_form.is_valid() and
-                instruction_form.is_valid()):
-            return self.form_valid(form, ingredient_form, instruction_form)
-        return self.form_invalid(form, ingredient_form, instruction_form)
-
-    def form_valid(self, form, ingredient_form, instruction_form):
+    def form_valid(self, form):
         form.instance.author = self.request.user
-        self.object = form.save()
-        for item in ingredient_form:
-            item.recipe = self.object
-            item.save()
-        for item in instruction_form:
-            item.recipe = self.object
-            item.save()
-        return HttpResponseRedirect(self.get_success_url())
+        context = self.get_context_data()
+        ingredients = context['ingredients']
+        instructions = context['instructions']
+        with transaction.atomic():
+            self.object = form.save()
 
-    def form_invalid(self, form, ingredient_form, instruction_form):
-        return self.render_to_response(
-            self.get_context_data(form=form,
-                                  ingredient_form=ingredient_form,
-                                  instruction_form=instruction_form))
+            if ingredients.is_valid() and instructions.is_valid():
+                ingredients.instance = self.object
+                instructions.instance = self.object
+                ingredients.save()
+                instructions.save()
+        return super(RecipeCreateView, self).form_valid(form)
 
 
 class RecipeUpdateView(generic.UpdateView):
@@ -74,47 +57,31 @@ class RecipeUpdateView(generic.UpdateView):
     template_name_suffix = '_update_form'
     success_url = '/'
 
-    def get(self, *args, **kwargs):
-        self.object = self.get_object()
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        ingredient_form = IngredientFormset(instance=self.object)
-        instruction_form = InstructionFormset(instance=self.object)
-        return self.render_to_response(
-            self.get_context_data(
-                form=form,
-                ingredient_form=ingredient_form,
-                instruction_form=instruction_form
-            )
-        )
+    def get_context_data(self, **kwargs):
+        data = super(RecipeUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['ingredients'] = IngredientFormset(
+                self.request.POST, instance=self.object)
+            data['instructions'] = InstructionFormset(
+                self.request.POST, instance=self.object)
+        else:
+            data['ingredients'] = IngredientFormset(instance=self.object)
+            data['instructions'] = InstructionFormset(instance=self.object)
+        return data
 
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        ingredient_form = IngredientFormset(
-            self.request.POST, instance=self.object)
-        instruction_form = InstructionFormset(
-            self.request.POST, instance=self.object)
+    def form_valid(self, form):
+        context = self.get_context_data()
+        ingredients = context['ingredients']
+        instructions = context['instructions']
+        with transaction.atomic():
+            self.object = form.save()
 
-        if (form.is_valid() and ingredient_form.is_valid() and
-                instruction_form.is_valid()):
-            return self.form_valid(form, ingredient_form, instruction_form)
-        return self.form_invalid(form, ingredient_form, instruction_form)
-
-    def form_valid(self, form, ingredient_form, instruction_form):
-        self.object = form.save()
-        ingredient_form.instance = self.object
-        ingredient_form.save()
-        instruction_form.instance = self.object
-        instruction_form.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-    def form_invalid(self, form, ingredient_form, instruction_form):
-        return self.render_to_response(
-            self.get_context_data(form=form,
-                                  ingredient_form=ingredient_form,
-                                  instruction_form=instruction_form))
+            if ingredients.is_valid() and instructions.is_valid():
+                ingredients.instance = self.object
+                instructions.instance = self.object
+                ingredients.save()
+                instructions.save()
+        return super(RecipeUpdateView, self).form_valid(form)
 
 
 class RecipeDeleteView(generic.DeleteView):
