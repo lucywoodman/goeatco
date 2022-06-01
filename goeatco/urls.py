a8fcf2b940718e4ad28e46300e8ee5304c7fd2f8
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-01 14:36:08 +0100

    feat[proj]: add favicon.ico and robots.txt as django keeps bugging me for them

--- goeatco/urls.py
+++ goeatco/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('recipe.urls')),
+    path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt", content_type="text/plain"))
 ]
