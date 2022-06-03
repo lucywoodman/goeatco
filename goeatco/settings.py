dff97b9531f7ffc4cdc6d7debcd369e133b337f8
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-03 17:11:19 +0100

    chore[rec]: install django-generic-filters instead of django-filters as it possibly works better with class-based views

--- goeatco/settings.py
+++ goeatco/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_bootstrap5',
-    'django_filters',
+    'django_generic_filters',
 
     # apps
     'recipe',
