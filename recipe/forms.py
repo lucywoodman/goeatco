dddca7adad84ea4a2620a66fd173bacaa27a6843
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-04 10:39:09 +0100

    feat[rec]: create a form for the ingredient 's category list

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, inlineformset_factory
 from django.utils.translation import gettext_lazy as _
-from .models import Ingredient, IngredientMeta, Recipe, Instructions
+from .models import Category, Ingredient, IngredientMeta, Recipe, Instructions
 
 
 class RecipeForm(ModelForm):
@@ -35,3 +35,16 @@ class IngredientForm(ModelForm):
     class Meta:
         model = Ingredient
         fields = ('__all__')
+
+
+class IngredientCategoryForm(ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ('category',)
+        labels = {
+            'category': _('Filter ingredients by category'),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.all()
