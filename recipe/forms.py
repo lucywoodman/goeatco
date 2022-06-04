69891b72b4ac1abbedbf35bfc3bac8cf7a2291e0
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-04 11:15:54 +0100

    feat[rec]: add short description to recipe model

--- recipe/forms.py
+++ recipe/forms.py
@@ -6,7 +6,8 @@ from .models import Category, Ingredient, IngredientMeta, Recipe, Instructions
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
-        fields = ('public', 'name', 'meal', 'cooking_time', 'featured_image')
+        fields = ('public', 'name', 'description', 'meal',
+                  'cooking_time', 'featured_image')
         exclude = ('slug', 'author')
         labels = {
             'name': _('Recipe name'),
