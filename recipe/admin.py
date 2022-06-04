d7894d83c36a01fc319260ec9c34d734d7ef1817
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-04 13:35:32 +0100

    feat[rec]: add categories to admin to allow for better management

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, IngredientMeta
+from .models import Category, Recipe, Ingredient, IngredientMeta
 
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(IngredientMeta)
+admin.site.register(Category)
