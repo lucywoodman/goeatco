562504e0a1fcf3b7e7ac6bb97786f0bfcef0cfe0
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-05 16:57:27 +0100

    chore[rec]: change to set_null so ingredients are not deleted if a category is removed

--- recipe/models.py
+++ recipe/models.py
@@ -64,7 +64,7 @@ class Category(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=80, unique=True)
     category = models.ForeignKey(
-        Category, related_name='categories', on_delete=models.CASCADE, null=True)
+        Category, related_name='categories', on_delete=models.SET_NULL, null=True)
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
