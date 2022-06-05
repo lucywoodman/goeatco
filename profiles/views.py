cfd139a2c6f74d1f0cb7d258c579c91a3629184a
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-05 20:06:00 +0100

    feat[pro]: create the first views for the profile details and update views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import ProfileForm
+from .models import Profile
 
-# Create your views here.
+
+class ProfileDetailView(generic.DetailView):
+    model = Profile
+
+
+class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    success_url = reverse_lazy('profile')
