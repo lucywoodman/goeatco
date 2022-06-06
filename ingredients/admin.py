8c1c4bec06fb7688c553ada8ef4fab0acd4110b5
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-06 17:14:41 +0100

    chore[ing]: move admin registerations for ingredients over to ingredients app

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ingredient, Category
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Category)
--- recipe/admin.py
+++ recipe/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Category, Recipe, Ingredient, IngredientMeta
-
+from .models import Recipe, IngredientMeta
 
 admin.site.register(Recipe)
-admin.site.register(Ingredient)
 admin.site.register(IngredientMeta)
-admin.site.register(Category)
