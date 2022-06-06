957cb93278f4c73ca74ce482ccafee7c0addb28f
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-06 17:10:10 +0100

    chore[ing]: move ingredient urls over to ingredients app and remove lead

--- recipe/urls.py
+++ recipe/urls.py
@@ -13,11 +13,4 @@ urlpatterns = [
     path('recipe/<slug:slug>/',
          views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipe/save/<slug:slug>', views.RecipeSave, name='recipe_save'),
-    path('ingredients/', views.IngredientListView.as_view(), name='ingredient_list'),
-    path('ingredients/create', views.IngredientCreateView.as_view(),
-         name='ingredient_create'),
-    path('ingredients/update/<int:pk>/',
-         views.IngredientUpdateView.as_view(), name='ingredient_update'),
-    path('ingredients/delete/<int:pk>/',
-         views.IngredientDeleteView.as_view(), name='ingredient_delete'),
 ]
