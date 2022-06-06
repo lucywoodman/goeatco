0002a23e7c9780bebe074c4ca4eacb68c18ec265
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-06 17:13:11 +0100

    chore[ing]: move ingredient views over to ingredients app for better management

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from .models import Ingredient
+from .forms import IngredientForm, IngredientCategoryForm
 
-# Create your views here.
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('category')
+        if query != None:
+            return qs.filter(category__pk=query)
+        else:
+            return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(IngredientListView, self).get_context_data(**kwargs)
+        context['form'] = IngredientCategoryForm()
+        return context
+
+
+class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Ingredient
+    form_class = IngredientForm
+    success_url = reverse_lazy('ingredient_list')
+
+
+class IngredientUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Ingredient
+    form_class = IngredientForm
+    success_url = reverse_lazy('ingredient_list')
+
+
+class IngredientDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Ingredient
+    template_name = '_confirm_delete.html'
+    success_url = reverse_lazy('ingredient_list')
--- recipe/views.py
+++ recipe/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.db.models import Q
-from .models import Ingredient, Recipe
-from .forms import IngredientCategoryForm, IngredientForm, RecipeForm, IngredientFormset, InstructionFormset
+from .models import Recipe
+from .forms import RecipeForm, IngredientFormset, InstructionFormset
 
 
 class HomeListView(generic.ListView):
@@ -130,41 +130,6 @@ class RecipeDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Recipe
     template_name = '_confirm_delete.html'
     success_url = reverse_lazy('recipe_list')
-
-
-class IngredientListView(LoginRequiredMixin, generic.ListView):
-    model = Ingredient
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        query = self.request.GET.get('category')
-        if query != None:
-            return qs.filter(category__pk=query)
-        else:
-            return qs
-
-    def get_context_data(self, **kwargs):
-        context = super(IngredientListView, self).get_context_data(**kwargs)
-        context['form'] = IngredientCategoryForm()
-        return context
-
-
-class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Ingredient
-    form_class = IngredientForm
-    success_url = reverse_lazy('ingredient_list')
-
-
-class IngredientUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Ingredient
-    form_class = IngredientForm
-    success_url = reverse_lazy('ingredient_list')
-
-
-class IngredientDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Ingredient
-    template_name = '_confirm_delete.html'
-    success_url = reverse_lazy('ingredient_list')
 
 
 class CookbookView(LoginRequiredMixin, generic.ListView):
