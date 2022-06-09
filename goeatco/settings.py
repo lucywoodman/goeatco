7e15ce73049a503c3c616fca357d081f91e7f4ff
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-09 20:02:06 +0100

    fix: enable email sending from django to fix error when registering with an email

--- goeatco/settings.py
+++ goeatco/settings.py
@@ -156,3 +156,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
