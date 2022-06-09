e1d7cf4357b071f92a37e957fab5258b9fe33cd1
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-09 22:21:02 +0100

    fix[pro]: remove pk from URLs as no longer needed with new profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('<int:pk>', views.ProfileDetailView.as_view(), name='profile'),
-    path('update/<int:pk>', views.ProfileUpdateView.as_view(), name='profile_update'),
+    path('', views.ProfileDetailView.as_view(), name='profile'),
+    path('update', views.ProfileUpdateView.as_view(), name='profile_update'),
 ]
